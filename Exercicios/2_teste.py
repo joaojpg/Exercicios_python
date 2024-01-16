9c8bc1f4a9a08f1403118ea4033325180da8e8f0
João Vitor <96800895+joaojpg@users.noreply.github.com>
2024-01-16 17:30:15 -0300

    teste_2_commit

--- Exercicios/2_teste.py
+++ Exercicios/2_teste.py
@@ -1,1 +1,7 @@
 print('teste de commit em dev')
+
+teste = input('digite algo para testar: ')
+
+print('foi isso que você digitou? "{}"'.format(teste))
+
+input('\nAperte enter para finalizar o programa.')
